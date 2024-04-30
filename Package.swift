4a69712f6dcc8ad4566009d425e0455834d90c42
fwcd <fwcd@fwcd.dev>
2024-04-30 02:12:12 +0200

    Export library XML

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "music2go",
+    platforms: [
+        .macOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .executable(
--- Sources/music2go/Music2Go.swift
+++ Sources/music2go/Music2Go.swift
@@ -1,10 +1,23 @@
 import ArgumentParser
+import Foundation
+import MusicLibrary
 
 @main
 struct Music2Go: ParsableCommand {
     static let configuration = CommandConfiguration(commandName: "music2go")
 
-    func run() {
-        print("Hello, world!")
+    @Option(name: .shortAndLong, help: "The path to the output folder.")
+    var output: String
+
+    func run() throws {
+        let outputURL = URL(filePath: output)
+        try FileManager.default.createDirectory(at: outputURL, withIntermediateDirectories: true)
+
+        let importer = try LocalAppleMediaImporter()
+        let library = try importer.readLibrary()
+
+        let xmlURL = outputURL.appending(component: "Library.xml")
+        let exporter = LibraryXMLExporter(url: xmlURL)
+        try exporter.write(library: library)
     }
 }
